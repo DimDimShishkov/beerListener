6f930dc7323eb61700326ff98e8d018dcc796281
Dmitry Shishkov <d.shishkov@ab-trade.ru>
2025-04-07 17:12:18 +0300

    вынес скрипт в отдельный файл

--- controllers/add.js
+++ controllers/add.js
@@ -1,4 +1,47 @@
-module.exports.addBar = (msgTxt) => {
-  console.log(msgTxt);
-};
+const puppeteer = require("puppeteer");
 
+// получаем название бара из URL
+async function getTitleByUrl(url) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  try {
+    await page.goto(url, { waitUntil: "networkidle0", timeout: 3000 });
+  } catch (e) {}
+
+  await page.content();
+
+  const title = await page.evaluate(() =>
+    document.title.replace(", Москва — Яндекс Карты", "")
+  );
+
+  await browser.close();
+  return title || "не вышло";
+}
+
+module.exports.addBar = async (msgTxt) => {
+  console.log(msgTxt);
+
+  // сперва разбиваем на строчки
+  // затем каждую строчку проверяем на наличие ссылки на гугл карты
+  // затем эту строчку чистим от остального текста
+  const url = msg.text
+    .split("\n")
+    .map((str) =>
+      str.includes("https://")
+        ? str.split(" ").find((el) => el.startsWith("https://"))
+        : ""
+    )
+    .find((str) => str.startsWith("https://"));
+
+  //TODO здесь вернуть сообщение в чат
+  if (!url)
+    return console.log(
+      "в сообщении нет ссылки. либо добавьте ссылку либо добавьте место через dev",
+      msg.text
+    );
+
+  const name = await getFileUrl(url);
+  messages.push({ name, url });
+  return console.log({ name, url });
+};
--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const TelegramBot = require("node-telegram-bot-api");
 const express = require("express");
 const http = require("http");
-
-const puppeteer = require("puppeteer");
+const { addBar } = require("./controllers/add");
 
 // Создаем бота
-const bot = new TelegramBot(process.env.TOKEN, {polling: true});
+const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
 // Создаем сервер с Express
 const app = express();
@@ -17,25 +16,6 @@ app.use(express.static("public"));
 // Массив для хранения баров
 let messages = [];
 
-// получаем название бара из URL
-async function getFileUrl(url) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-  try {
-    await page.goto(url, {waitUntil: "networkidle0", timeout: 3000});
-  } catch (e) {}
-
-  await page.content();
-
-  const title = await page.evaluate(() =>
-    document.title.replace(", Москва — Яндекс Карты", ""),
-  );
-
-  await browser.close();
-  return title || "не вышло";
-}
-
 // Обработчик получения сообщений из Telegram
 bot.on("message", async (msg) => {
   // если со стикером, картинкой итд
@@ -44,29 +24,12 @@ bot.on("message", async (msg) => {
   // если в сообщении нет тега #место
   if (!msg.text.match(/#место/gi)) return;
 
-  // сперва разбиваем на строчки
-  // затем каждую строчку проверяем на наличие ссылки на гугл карты
-  // затем эту строчку чистим от остального текста
-  const url = msg.text
-    .split("\n")
-    .map((str) =>
-      str.includes("https://")
-        ? str.split(" ").find((el) => el.startsWith("https://"))
-        : "",
-    )
-    .find((str) => str.startsWith("https://"));
-  if (url) {
-    const name = await getFileUrl(url);
-    messages.push({name, url});
-    return console.log({name, url});
-  }
-  messages.push({name: "место не распознано", url: msg.text});
-  // вернуть в чат сообщение "место не распознано"
-  return console.log("место не распознано", msg.text);
+  const name = await addBar(msg.text);
+  messages.push({ name, url });
+  return console.log({ name, url });
 });
 
 // Запуск сервера на порту 3000
 server.listen(3001, () => {
   console.log("Server started on http://localhost:3001");
 });
-
